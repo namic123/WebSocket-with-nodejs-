02de4b57d884ebc6ba43e0eca7f0b71aae70c5cc
jay sung <namic123@naver.com>
2024-02-18 02:47:27 +0900

    서버와 클라이언트 websocket 연결.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,3 @@
-alert("hi");
+// 웹소켓 연결 및 현재 host 정보.
+// websocket은 http처럼 프로토콜이기 때문에 scheme을 ws로 연결해야함.
+const socket = new WebSocket(`ws://${window.location.host}`);
--- src/server.js
+++ src/server.js
@@ -8,12 +8,21 @@ app.set("view engine","pug");
 app.set("views",__dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/", (req, res)=> res.render("home"))
-app.get("/*", (req, res)=> res.redirect("/"));
+app.get("/", (_, res)=> res.render("home"))
+app.get("/*", (_, res)=> res.redirect("/"));
 const handleListen = () => console.log("Listening on http://localhost:3000");
 // http 서버와 websocket 서버를 동시에 실행
 // http 서버
 const server = http.createServer(app);
 // websocket 서버
 const wss = new WebSocket.Server({server});
+
+// 웹 소켓의 이벤트들(addEventListener와 유사)
+// 1번 파라미터에는 이벤트, 2번에는 콜백 함수
+// 콜백 함수의 파라미터에는 연결 정보가 포함된다.
+function handleConnection(socket){
+  console.log(socket);
+}
+// "연결" 이벤트를 감지
+wss.on("connection",handleConnection)
 server.listen(3000, handleListen);
