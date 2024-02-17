c3f597e554d8e672e94557ddc42f5957d3d81277
jay sung <namic123@naver.com>
2024-02-18 03:26:21 +0900

    서버와 클라이언트 websocket 연결하여, 다양한 이벤트 감지. 클라이언트 , 서버 socket 연결, 종료, 데이터 보내기 등 기본 기능 사용

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,24 @@
 // 웹소켓 연결 및 현재 host 정보.
 // websocket은 http처럼 프로토콜이기 때문에 scheme을 ws로 연결해야함.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+// 소켓이 연결되었음을 감지
+socket.addEventListener("open",()=>{
+  console.log("Connected to Server : 연결완료");
+});
+
+// 서버에서 보내진 message를 감지
+socket.addEventListener("message", (message) =>{
+  console.log("Just got this : ", message , "from the server");
+  console.log("서버에게 온 메세지:", message.data);
+});
+
+// 서버에서 소켓 연결을 종료한 것을 감지
+socket.addEventListener("close", ()=>{
+  console.log("Connected from Server: 연결종료")
+});
+
+// 서버에게 데이터 보내기
+setTimeout(()=> {
+  socket.send("hello from the browser");
+},10000);
--- src/server.js
+++ src/server.js
@@ -20,9 +20,19 @@ const wss = new WebSocket.Server({server});
 // 웹 소켓의 이벤트들(addEventListener와 유사)
 // 1번 파라미터에는 이벤트, 2번에는 콜백 함수
 // 콜백 함수의 파라미터에는 연결 정보가 포함된다.
-function handleConnection(socket){
-  console.log(socket);
-}
+
 // "연결" 이벤트를 감지
-wss.on("connection",handleConnection)
+wss.on("connection", (socket) => {
+  // 클라이언트 브라우저에 연결되어 있음을 알림
+  console.log("Connected to Browser : 연결완료");
+  // 클라이언트 브라우저가 닫힌 것을 감지
+  socket.on("close",()=> console.log("Disconnected from the Browser : 클라이언트가 종료했습니다."));
+  // 클라이언트에서 보낸 message를 감지
+  socket.on("message", message => {
+    // message를 utf-8 형식으로 변환
+    const translatedMessageData = message.toString('utf8');
+    console.log(translatedMessageData);
+  })
+  socket.send("hello!!");
+})
 server.listen(3000, handleListen);
